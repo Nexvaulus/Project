2e18d24c244f3b1dc87746c78dff9a9833610419
Nexvaulus <moehammad.fikr@gmail.com>
2021-10-12 19:36:17 +0700

    Menambahkan paragraf

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Yoi")
+var nama = ["ilham", "reza", "ucup", "udin"]
+
+var sebutNama = function(x) {
+    for(var i = 0; i < x.length; i++) {
+        return console.log(x[i])
+    }
+} 
+
+console.log(sebutNama(nama))
