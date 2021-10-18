6ff4c10631c898f17ad87e6d2db3582ac4910481
Nexvaulus <moehammad.fikr@gmail.com>
2021-10-18 15:42:16 +0700

    coba function prima di mtk.js

--- mtk.js
+++ mtk.js
@@ -1,12 +1,12 @@
 var angka;
 var prima = function(angka) {
-    for(i = 0; i < angka; i++) {
+    for(i = 2; i <= angka; i++) {
         var tes;
-        tes = angka % i;
+        tes = angka / i;
         if(tes == 1) {
             return console.log(angka + " adalah bilangan prima");
         }
-        else {
+        else { 
             return console.log(angka + " bukan bilangan prima");
         }
     }
