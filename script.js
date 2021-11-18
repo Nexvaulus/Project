e85ed34c67ba74a78e1a2a37383cc0b0dd66843e
Nexvaulus <moehammad.fikr@gmail.com>
2021-11-18 13:24:08 +0700

    Menambahkan js DOM

--- script.js
+++ script.js
@@ -2,30 +2,42 @@
 
 
 const box = document.querySelector('.box');
-box.addEventListener('click', function() {
-    let satu = "size";
-    let dua = "caption";
-
-    if(this.classList.contains(satu)) [satu, dua] = [dua, satu];
-    this.classList.toggle(satu);
-    setTimeout(() => {
-        this.classList.toggle(dua)
-    }, 700);
-});
-
-
 const center = document.querySelector(".center");
 const merah = center.querySelector('input[name=sMerah]');
 const hijau = center.querySelector('input[name=sHijau]');
 const biru = center.querySelector('input[name=sBiru]');
+
+
+box.addEventListener('click', function() {
+    let satu = "size";
+    let dua = "caption";
+
+    if(this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu];
+
+    }
+    this.classList.toggle(satu);
+    setTimeout(() => {
+        this.classList.toggle(dua)
+    }, 700);
+
+
+    
+    
+    
+});
+
+
 center.addEventListener('input', function(e) {
 
     const r = merah.value;
     const g = hijau.value;
     const b = biru.value;
 
+    merah.nextElementSibling.innerHTML = r;
+    hijau.nextElementSibling.innerHTML = g;
+    biru.nextElementSibling.innerHTML = b;
+
     e.target.parentElement.parentElement.parentElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    // e.target.parentElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    // e.target.siblingElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
 });
